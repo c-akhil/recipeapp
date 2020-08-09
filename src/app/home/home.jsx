0a46c28e8311ded3df62db7b5c5d9e393dce31b2
Akhil Kumar <akhil_kumar@Akhil-MacBook-Air-9.local>
2020-08-09 22:25:54 +0530

    1st screen

--- src/app/home/home.jsx
+++ src/app/home/home.jsx
@@ -15,6 +15,15 @@ class HomeComponet extends React.Component {
     componentDidMount() {
         httpSevice.get(apiendpoints.GET_RECIPE_LIST).then((response) => {
             let recipeList = response.data;
+            recipeList.forEach((recipe) => {
+                if (recipe.duration === undefined) {
+                    recipe.duration = Math.floor(Math.random() * 10);
+                }
+                if (recipe.isFavourite === undefined) {
+                    let ran = Math.floor(Math.random() * 10);
+                    recipe.isFavourite = ran >= 5;
+                }
+            })
             this.props.setRecipeList(recipeList);
         })
     }
@@ -35,21 +44,37 @@ class HomeComponet extends React.Component {
         // "description":"A unique combination of Indian Uthappam (pancake) and Italian pizza"
 
 
-        let recipeListUI = [...this.props?.home?.recipeList].map((recipe, index) => <React.Fragment key={"recepeList" + index}>
-            <div className="card" style={{ "background-image": `url(${recipe.image})`}}>
-                {recipe.category && <span className="category">In {recipe.category}</span>}
-                {/* <img className="card-img-top" src={recipe.image} alt={recipe.name} />
-                 */}
-                <div class="card-header" >
-                    
-                </div>
-                <div className={"card-body " + (index % 2 == 0 ? 'card-body-even' : 'card-body-odd')}>
-                    <h5 className="card-title">{recipe.name}</h5>
-                    <p className="card-text">{recipe.description}</p>
-                </div>
-            </div>
+        let recipeListUI = [...this.props?.home?.recipeList].map((recipe, index) => {
+            return <React.Fragment key={"recepeList" + index}>
+                <div className="card" style={{ "backgroundImage": `url(${recipe.image})` }}>
+                    {recipe.category && <span className="category">In {recipe.category}</span>}
+                    <div className="card-container">
 
-        </React.Fragment>);
+                        <div class="card-header" >
+                            <div className="display-on-hover">
+                                <button type="button" class="btn hover-btn btn-lg btn-block">View More</button>
+                                <button type="button" class="btn hover-btn btn-lg btn-block">Quick View</button>
+                            </div>
+                        </div>
+                        <div className={"card-body text-left " + (index % 2 == 0 ? 'card-body-even' : 'card-body-odd')}>
+                            <div className="row">
+                                <h5 className="col-10 card-title">{recipe.name}</h5>
+                                <img className="heart-icon" onClick={() => {
+                                    let recipeList = this.props.home.recipeList;
+                                    recipeList[index].isFavourite = !recipeList[index].isFavourite;
+                                    this.props.setRecipeList(recipeList);
+                                }} src={recipe.isFavourite ? "/assets/Icons/Icon feather-heart.png" : "/assets/Icons/Icon feather-heart-color.png"} />
+                            </div>
+                            <p>
+                                <img className="clock-icon" src="/assets/Icons/Icon feather-clock.png" />
+                                {recipe.duration + " min"}
+                            </p>
+                            <p className="card-text">{recipe.description}</p>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        });
         return <React.Fragment>
             <div className="card-columns">
                 {recipeListUI}
